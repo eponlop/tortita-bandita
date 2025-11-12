61000daaf7c820fd45e75e1c8a31f6b82588ba0b
Multimotivation <168669616+Multimotivation@users.noreply.github.com>
2025-11-12 22:58:31 +0100

    Correr, texturas varias y importacion de assets

--- Primer_Nivel/Assets/Scripts/PlayerController.cs
+++ Primer_Nivel/Assets/Scripts/PlayerController.cs
@@ -1,37 +1,66 @@
-using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
-
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    private float currentSpeed;
+    private bool isRunning;
+
     private CharacterController controller;
     [SerializeField] private Transform camera;
     public float rotSpeed = 15.0f;
     private Vector2 moveInput;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        
+        currentSpeed = walkSpeed;
     }
+
+    // Movimiento (Input del joystick o teclado)
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        Debug.Log("Move: " + moveInput);
+    }
+
+    // Correr (Input de botón)
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isRunning = true;
+            Debug.Log("Running");
+        }
+
+        else if (context.canceled)
+        {
+            isRunning = false;
+            Debug.Log("BackToWalk");
+        }
     }
 
     private void Update()
     {
-        Vector3 horizVel = new Vector3(moveInput.x, 0, moveInput.y) * speed;
+        // Selecciona la velocidad según el estado
+        currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        // Movimiento relativo a la cámara
+        Vector3 horizVel = new Vector3(moveInput.x, 0, moveInput.y) * currentSpeed;
+
         if (!Mathf.Approximately(horizVel.magnitude, 0.0f))
         {
             Quaternion rot = Quaternion.Euler(0, camera.eulerAngles.y, 0);
             horizVel = rot * horizVel;
             Quaternion direction = Quaternion.LookRotation(horizVel);
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
-
         }
+
         controller.Move(horizVel * Time.deltaTime);
+
+        // Aplicar gravedad
+        controller.Move(Physics.gravity * Time.deltaTime);
     }
 }
